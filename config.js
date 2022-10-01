9a80bdbd2c86309b8c6da92eeae5cbbf60022ad8
H.Anh <99712037+anhtlauncher@users.noreply.github.com>
2022-10-01 11:59:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = exports = {
-    "bot_token": "MTAxODg3ODY3MzMxMjU0Njg3Ng.G0eDUu.K24ml32yZpnN6vjckx0V5MlaYhNXNfH306rII8", // bot token
+    "bot_token": "", // bot token
     "prefix": "!", // bot prefix
     "id_length": 12, // length of file ids
     "max_file_size": 1e+7, // max file size in bytes
